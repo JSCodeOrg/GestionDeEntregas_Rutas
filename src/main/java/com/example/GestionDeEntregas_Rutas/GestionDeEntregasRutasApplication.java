29d94aa9a5c7a0108ae74879c2f685544fb7ebc4
StivenRuiz2005 <carlosruizrojas2005@gmail.com>
2025-05-23 15:48:06 -0500

    first commit

---
+++ src/main/java/com/example/GestionDeEntregas_Rutas/GestionDeEntregasRutasApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionDeEntregas_Rutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeEntregasRutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeEntregasRutasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestionDeEntregas_Rutas/GestionDeEntregasRutasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionDeEntregas_Rutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEntregasRutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
